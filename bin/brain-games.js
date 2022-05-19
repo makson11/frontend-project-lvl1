4277f4a4403b8d60375f9db58e858a2560d79bd3
makson11 <mamelnikov94@gmail.com>
2022-05-19 19:44:04 +1000

    The fourth step on the Hexlet project

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,4 @@
-#!/usr/bin/env node
-import {getsTheUserName} from '../src/cli.js';
+import getsTheUserName from '../src/cli.js';
 
-console.log("Welcome to the Braine Games!");
+console.log('Welcom to the Brain Games!');
 console.log(getsTheUserName());
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const getsTheUserName = () => {
-    const name = readlineSync.question('May I have your name?');
-    return `Hello, ${name}!`;
+const getsTheUserName = () => {
+  const name = readlineSync.question('May I have your name?');
+  return `Hello, ${name}!`;
 };
+
+export default getsTheUserName;
