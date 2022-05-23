ac0ec9b774ce89cb567832bae1c306d35e911b8d
Maxim Melnikov <mamelnikov94@gmail.com>
2022-05-23 13:21:49 +1000

    The first game on the Hexlet project

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,24 +1,48 @@
+import readlineSync from 'readline-sync';
+
 // случайное число от min до (max+1)
 const randomInteger = (min, max) => {
-    let rand = min + Math.random() * (max + 1 - min);
-    return Math.floor(rand);
-}
+  const rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+};
 
 const tellsTheRulesOfTheGme = () => {
-    const name = readlineSync.question('May I have your name?');
-    const rules = 'Answer "yes" it the number is even, otherwise answer "on"';
-    console.log(rules);
+  const name = readlineSync.question('May I have your name?');
+  const rules = 'Answer "yes" it the number is even, otherwise answer "on"';
 
+  console.log(name);
+  console.log(rules);
+
+  let i = 0;
+  do {
     const randomNumb = randomInteger(1, 100);
 
-    let i = 1;
-    do {
-        console.log(`Question: ${randomNumb()}`);
-        const answerToTheQuestion = readlineSync.question('Your answer: ');
-        if(randomNumb() % 2 === 0 && answerToTheQuestion === 'Yes'){
-            console.log('Correct!');
-        }
-        i++;
-    } while (i <= 3);
+    console.log(`Question: ${randomNumb}`);
 
+    const answerToTheQuestion = readlineSync.question('Your answer: ');
+    if (randomNumb % 2 === 0) {
+      if (answerToTheQuestion === 'yes') {
+        console.log('Correct!');
+      } else {
+        console.log(`'${answerToTheQuestion}' is wrong answer ;(. Correct answer was 'yes'.Let's try again, ${name}`);
+        break;
+      }
+    }
+
+    if (randomNumb % 2 !== 0) {
+      if (answerToTheQuestion === 'no') {
+        console.log('Correct!');
+      } else {
+        console.log(`'${answerToTheQuestion}' is wrong answer ;(. Correct answer was 'no'.Let's try again, ${name}`);
+        break;
+      }
+    }
+
+    if (i === 2) {
+      console.log(`Congratulations, ${name}`);
+    }
+    i += 1;
+  } while (i <= 2);
 };
+
+tellsTheRulesOfTheGme();
