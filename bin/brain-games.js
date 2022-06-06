57ad2668f02d884981ec4dbb1d912317c0166e84
Maxim Melnikov <mamelnikov94@gmail.com>
2022-06-06 16:05:21 +1000

    edits

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,3 @@
 import getsTheUserName from '../src/cli.js';
 
-console.log('Welcom to the Brain Games!');
 console.log(getsTheUserName());
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
 const getsTheUserName = () => {
+  console.log('Welcom to the Brain Games!');
   const name = readlineSync.question('May I have your name?');
   return `Hello, ${name}!`;
 };
