4c72d67857de814d510dac158ce05357add7afcf
Maxim Melnikov <mamelnikov94@gmail.com>
2022-06-09 19:04:38 +1000

    edits

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 const welcome = () => {
   console.log('Welcom to the Brain Games!');
   const name = readlineSync.question('May I have your name?');
-  return `Hello, ${name}!`;
+  return name;
 };
 
 export default welcome;
